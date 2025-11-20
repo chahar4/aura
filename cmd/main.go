4afda5a7d35d588b18e01d8e8f3a90f44ad5e387
chahar4 <rezacharsetad@outlook.com>
2025-11-20 15:17:41 +0330

    reset

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"log"
 
-	"github.com/chahar4/aura/db"
-	"github.com/chahar4/aura/internal/repository"
 )
 
 func main() {
-	db, err := db.NewDatabase()
-	if err != nil {
 
-		log.Fatalln(err.Error())
-	}
-	userRepo := repository.NewUserRepository(db.GetDB())
-	userRepo.Init()
 }
